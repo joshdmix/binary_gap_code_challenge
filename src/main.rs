bfa9d8bff46bbb68db4149e2cb9e658edb8647db
Josh Mix <josh.mix@eltoro.com>
2020-01-25 19:47:30 -0500

    max bin gap moved to own function

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,20 @@ fn main() {
         )
         .get_matches();
     let input = matches.value_of("integer").unwrap();
+    calculate_max_binary_gap(input);
+}
 
+fn calculate_max_binary_gap(input: &str) {
     let parse_input = input
         .parse::<i64>()
-        .expect("******Invalid input. Please enter an integer.******");
+        .expect("******Invalid input. Please enter an integer between -9223372036854775807 and 9223372036854775807.******");
     let input_bin: String = format!("{:b}", parse_input);
-    let trim_bin: &str = input_bin.trim_start_matches('0').trim_end_matches('0');
-    let split_input = trim_bin.split("1").collect::<Vec<&str>>();
-    let len_vec: Vec<_> = split_input.iter().map(|gap| gap.len()).collect();
+    let trim_split_bin: Vec<&str> = input_bin
+        .trim_start_matches('0')
+        .trim_end_matches('0')
+        .split("1")
+        .collect::<Vec<&str>>();
+    let len_vec: Vec<_> = trim_split_bin.iter().map(|gap| gap.len()).collect();
     let max_gap = len_vec.iter().max().unwrap();
     println!("Your number {:?}", parse_input);
     println!("Your number in binary {:?}", input_bin);
